f071988443c52922d75157c4e2767a3ae0c01447
Mattie <MattieCanete@gmail.com>
2022-04-12 19:51:22 -0700

    heroku

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///capstone_1'))
+app.config['SQLALCHEMY_DATABASE_URI'] = (uri)
+    # os.environ.get('DATABASE_URL', 'postgresql:///capstone_1'))
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
